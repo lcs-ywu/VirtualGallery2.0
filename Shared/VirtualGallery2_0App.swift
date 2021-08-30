1ab69dd207fc88ba92bfaa6154c9403b2108a0fb
James Wu <ywu@lcs.on.ca>
2021-08-30 12:12:57 +0800

    Initial Commit

---
+++ Shared/VirtualGallery2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualGallery2_0App.swift
+//  Shared
+//
+//  Created by James Wu on 2021-08-30.
+//
+
+import SwiftUI
+
+@main
+struct VirtualGallery2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
