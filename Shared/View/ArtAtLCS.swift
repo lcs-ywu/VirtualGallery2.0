b99f22a0469ed4be4cd767cfefb9e04e85af9f12
James Wu <ywu@lcs.on.ca>
2021-08-30 12:38:06 +0800

    Add a view file and push the changes

---
+++ Shared/View/ArtAtLCS.swift
@@ -0,0 +1,20 @@
+//
+//  ArtAtLCS.swift
+//  ArtAtLCS
+//
+//  Created by James Wu on 2021-08-30.
+//
+
+import SwiftUI
+
+struct ArtAtLCS: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArtAtLCS_Previews: PreviewProvider {
+    static var previews: some View {
+        ArtAtLCS()
+    }
+}
